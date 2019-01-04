eef38a59775b07d577349627f324fd83e6487875
Andrew Hyatt <4400272+ahyattdev@users.noreply.github.com>
2019-01-04 13:41:29 -0500

    Event changed

--- site.sh
+++ site.sh
@@ -2,14 +2,20 @@
 
 set -e
 
+echo "Looking at previous hash"
+
 go run event_changed.go
 
-if  [ $? == 1 ]
-then
+status=$?
+echo "Exit status is $status"
+if echo hi; then
+	echo "Generating site site"
 	# Generate site
 	go run generate.go > docs/index.html
 	# Commit it
 	git commit -am "Event changed"
 	# Push it
 	git push
+else
+	echo "Nothing to do"
 fi
