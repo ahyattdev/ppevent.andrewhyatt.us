e6c7f7e60da59206cbd3d454c6f2d18aa8c01583
Andrew Hyatt <4400272+ahyattdev@users.noreply.github.com>
2019-09-03 11:46:57 -0400

    Update generation script

--- site.sh
+++ site.sh
@@ -4,12 +4,19 @@ set -e
 
 date
 
-cd ~/git/ppevent.andrewhyatt.us
+cd "$(dirname "$0")"
 
-if /usr/local/bin/go run event_changed.go; then
+git config user.name "Andrew Hyatt"
+git config user.email "4400272+ahyattdev@users.noreply.github.com"
+
+export GO111MODULE=on
+
+go get
+
+if go run event_changed.go; then
 	echo "Generating site"
 	# Generate site
-	/usr/local/bin/go run generate.go > docs/index.html
+	go run generate.go > docs/index.html
 	# Commit it
 	git commit -am "Event changed"
 	# Push it
